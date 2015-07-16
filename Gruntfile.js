e20ef1ea46d44b640f55010c1e54dac9ada90c4b
no <studio@larrymite.com.au>
2015-07-16 10:28:07 +1000

    app

---
+++ Gruntfile.js
@@ -0,0 +1,35 @@
+module.exports = function(grunt) {
+
+  // Project configuration.
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    less: {
+      development: {
+        options: {
+          compress: true,
+          yuicompress: true,
+          optimization: 2
+        },
+        files: {
+          "www/css/index.css": "www/css/index.less" // destination file and source file
+        }
+      }
+    },
+    watch: {
+      styles: {
+        files: ['www/css/*.less'], // which files to watch
+        tasks: ['less'],
+        options: {
+          nospawn: true
+        }
+      }
+    }
+  });
+
+  // Load the plugin that provides the "uglify" task.
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-less');
+  // Default task(s).
+  grunt.registerTask('default', ['less', 'watch']);
+
+};
---
+++ www/js/index.js
@@ -0,0 +1,33 @@
+var app = {
+    initialize: function() {
+        this.bindEvents();
+    },
+   
+    bindEvents: function() {
+        document.addEventListener('deviceready', this.onDeviceReady, false);
+    },
+
+    onDeviceReady: function() {
+        app.receivedEvent('deviceready');
+		
+    },
+ 
+    receivedEvent: function(id) {
+        var parentElement = document.getElementById(id);
+        var listeningElement = parentElement.querySelector('.listening');
+        var receivedElement = parentElement.querySelector('.received');
+        listeningElement.setAttribute('style', 'display:none;');
+        receivedElement.setAttribute('style', 'display:block;');
+        console.log('Received Event: ' + id);
+    }
+	
+};
+
+$(document).ready(function(){
+	$(".back").click(function(){
+		window.history.back();
+	});
+	$(".home").click(function(){
+		window.location = "index.html";
+	});
+});
